3d8be817d057ee51b74ef811a2edf8350d22f89b
Emilio Ochoa <emilioaor@gmail.com>
2021-09-08 00:52:05 -0400

    Removing dump

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -23,13 +23,11 @@ class ProductSeeder extends Seeder
             for ($x = 1; $x <= 10; $x++) {
 
                 foreach ($colors as $i => $color) {
-                    dump('Voy a crear');
                     $product = new Product();
                     $product->model = 'MD' . $x;
                     $product->description = 'MD' . $x . ' ' . $color;
                     $product->brand_id = $brand->id;
                     $product->save();
-                    dump('Listo');
                 }
             }
         }
